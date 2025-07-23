7cef144fbeae29728e33578c5c8889de46490df3
mandil malla <mallamandilcoding@gmail.com>
2025-07-23 23:15:18 +0900

    routes beginner

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
-import Meal from "./FoodRecipie/Meal";
-import NavigationBar from "./FoodRecipie/NavigationBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Team from "./pages/Team";
+
+// import Meal from "./FoodRecipie/Meal";
+// import NavigationBar from "./FoodRecipie/NavigationBar";
 
 const App = () => {
-  const [category, setCategoryText] = useState("canadian");
-  const [searchText, setSearchText] = useState("");
+  // const [category, setCategoryText] = useState("canadian");
+  // const [searchText, setSearchText] = useState("");
   // console.log(searchText);
+
   return (
     <>
-      <NavigationBar
+      {/* <NavigationBar
         setCategory={setCategoryText}
         setSearchText={setSearchText}
       />
-      <Meal category={category} searchText={searchText} />
+      <Meal category={category} searchText={searchText} /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/product" element={<Product />}></Route>
+          <Route path="/team" element={<Team />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 };
