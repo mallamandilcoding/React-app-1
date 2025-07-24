8f6ad3dbe0155984c9475aff8dcefa43fc7e9be8
mandil malla <mallamandilcoding@gmail.com>
2025-07-24 22:02:27 +0900

    dynamic routing part-1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
 import Team from "./pages/Team";
+import ProductDetail from "./pages/ProductDetail";
 
 // import Meal from "./FoodRecipie/Meal";
 // import NavigationBar from "./FoodRecipie/NavigationBar";
@@ -25,9 +26,10 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
-          <Route path="/product" element={<Product />}></Route>
+          <Route path="/products" element={<Product />}></Route>
           <Route path="/team" element={<Team />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/products/:id" element={<ProductDetail />}></Route>
         </Routes>
       </Router>
     </>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Product = () => {
-  return <div>Product</div>;
+  const [apidata, setApiData] = useState([]);
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      const api = await fetch("https://fakestoreapi.com/products");
+      const data = await api.json();
+      setApiData(data);
+      // console.log("my data = ", data);
+    };
+    fetchDataFromApi();
+  }, []);
+
+  return (
+    <>
+      <div>
+        {apidata.map((data) => (
+          <div>
+            <Link to={`/products/${data.id}`}>{data.title}</Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Product;
